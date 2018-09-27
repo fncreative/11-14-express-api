1c0dbd9ea538b271bc9c4b03cc33701029957562
Daniel Frey <dann.frey@me.com>
2018-09-26 21:48:31 -0700

    added the rest of the tests

--- src/__test__/album-router.test.js
+++ src/__test__/album-router.test.js
@@ -11,8 +11,9 @@ const API_URL = `http://localhost:${process.env.PORT}/api/albums`;
 describe('api/albums', () => {
   beforeAll(server.start);
   afterAll(server.stop);
-  // beforeEach(albumMock.pCleanAlbumMocks);
+  beforeEach(albumMock.pCleanAlbumMocks);
 
+  // POST test
   test('this should respond with 200 status code and a new json album', () => {
     const originalRequest = {
       title: faker.lorem.words(3),
@@ -29,39 +30,33 @@ describe('api/albums', () => {
         expect(response.body.timestamp).toBeTruthy();
       });
   });
-  test('this should respond with a 200 status code and a json album if there is a matching id', () => {
-    let savedAlbumMock = null;
-    return albumMock.pCreateAlbumMock()
-      .then((createdAlbumMock) => {
-        savedAlbumMock = createdAlbumMock;
-        return superagent.get(`${API_URL}/${createdAlbumMock._id}`);
-      })
-      .then((getResponse) => {
-        expect(getResponse.status).toEqual(200);
-        expect(getResponse.body.timestamp).toBeTruthy();
-        expect(getResponse.body._id.toString()).toEqual(savedAlbumMock._id.toString());
-        expect(getResponse.body.title).toEqual(savedAlbumMock.title);
-      });
-  });
-  test('this should respond with 200 status code and a json album if there is a matching id', () => {
+  // POST failure test
+  test('this should respond with 400 status code if there is no album title', () => {
     const originalRequest = {
-      title: faker.lorem.words(3),
-      year: faker.random.number(),
+      content: faker.lorem.words(2),
     };
     return superagent.post(API_URL)
       .set('Content-Type', 'application/json')
       .send(originalRequest)
-      .then((postResponse) => {
-        originalRequest._id = postResponse.body._id;
-        return superagent.get(`${API_URL}/${postResponse.body._id}`);
-      })
-      .then((getResponse) => {
-        expect(getResponse.status).toEqual(200);
-        expect(getResponse.body.timestamp).toBeTruthy();
-        expect(getResponse.body._id.toString()).toEqual(originalRequest._id);
-        expect(getResponse.body.title).toEqual(originalRequest.title);
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(400);
       });
   });
+  // Post failure test
+  test('this should respond with 400 status code if there is no content at all', () => {
+    const originalRequest = {
+      title: faker.lorem.words(2),
+    };
+    return superagent.post(API_URL)
+      .set('Content-Type', 'application/json')
+      .send(originalRequest)
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(400);
+      });
+  });
+  // Delete test
   test('this should respond with 204 status code', () => {
     const originalRequest = {
       title: faker.lorem.words(3),
@@ -76,6 +71,67 @@ describe('api/albums', () => {
       })
       .then((getResponse) => {
         expect(getResponse.status).toEqual(200);
+      });
+  });
+  // Delete failure test
+  test('should respond with 400 if the delete route is not matched', () => {
+    return superagent.delete(`${API_URL}/davematthews`)
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status)
+          .toEqual(404);
+      });
+  });
+  // GET :id test
+  test('this should respond with a 200 status code and a json album if there is a matching id', () => {
+    let savedAlbumMock = null;
+    return albumMock.pCreateAlbumMock()
+      .then((createdAlbumMock) => {
+        savedAlbumMock = createdAlbumMock;
+        return superagent.get(`${API_URL}/${createdAlbumMock._id}`);
+      })
+      .then((getResponse) => {
+        expect(getResponse.status).toEqual(200);
+        expect(getResponse.body.timestamp).toBeTruthy();
+        expect(getResponse.body._id.toString()).toEqual(savedAlbumMock._id.toString());
+        expect(getResponse.body.title).toEqual(savedAlbumMock.title);
+      });
+  });
+  // Get failure test
+  test('should respond with 400 if the get route is not matched', () => {
+    return superagent.get(`${API_URL}/davematthews`)
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status)
+          .toEqual(404);
+      });
+  });
+  // PUT test
+  test('should respond with 200 if we updated an album title and year', () => {
+    let savedAlbumMock = null;
+    return albumMock.pCreateAlbumMock()
+      .then((createdAlbumMock) => {
+        savedAlbumMock = createdAlbumMock;
+        const newPutRequest = {
+          title: faker.lorem.words(1),
+          year: faker.random.number(),
+        };
+        return superagent.put(`${API_URL}/${createdAlbumMock._id}`)
+          .send(newPutRequest)
+          .then((putResponse) => {
+            expect(putResponse.status).toEqual(200);
+            expect(putResponse.body._id.toString()).toEqual(savedAlbumMock._id.toString());
+            expect(putResponse.body.title).toEqual(newPutRequest.title);
+            expect(putResponse.body.year).toEqual(newPutRequest.year);
+          });
+      });
+  });
+  // PUT failure test
+  test('this should respond with a 404 error if there is not an id to update', () => {
+    return superagent.put(`${API_URL}/davematthews`)
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(404);
       });
   });
 });
